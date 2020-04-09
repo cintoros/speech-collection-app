0ad0fc495b86f7fe40a62761c5c10d13c003e27e
Elias Schorr <elias.schorr@fhnw.ch>
2020-04-09 15:57:58 +0200

    re-add feature to label while recording/transcribing

--- backend/src/main/resources/db/migration/V8__refactor_db.sql
+++ backend/src/main/resources/db/migration/V8__refactor_db.sql
@@ -1,7 +1,20 @@
 -- refactor database for audio,image,text matching between each other.
 
 -- TODO not sure if it makes sense to migrate or if we want to re-create the database?
+
 -- TODO probably add triggers to update occurrence counts on insert and delete (or join each time for overviews) once the base data structure is clear.
+create table labeled_data
+(
+    id                BIGINT NOT NULL AUTO_INCREMENT,
+    is_sentence_error BOOLEAN DEFAULT FALSE,
+    is_private        BOOLEAN DEFAULT FALSE,
+    correct           BIGINT  DEFAULT 0,
+    wrong             BIGINT  DEFAULT 0,
+    skipped           BIGINT  DEFAULT 0,
+    PRIMARY KEY (id)
+) ENGINE = INNODB
+  DEFAULT CHARSET = UTF8MB4
+    COMMENT 'this table is used to save aggregated checked_states of the audio,text, images and occurrences.';
 
 RENAME TABLE source TO transcript_source;
 
@@ -30,14 +43,16 @@ or it can be imported/upload by a script etc this is why the more "advanced" fie
 
 CREATE TABLE text
 (
-    id           BIGINT   NOT NULL AUTO_INCREMENT,
-    text         TEXT     NOT NULL,
-    source_id    BIGINT   NOT NULL,
-    language_id  BIGINT   NOT NULL,
-    created_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id              BIGINT   NOT NULL AUTO_INCREMENT,
+    text            TEXT     NOT NULL,
+    source_id       BIGINT   NOT NULL,
+    language_id     BIGINT   NOT NULL,
+    created_time    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    labeled_data_id BIGINT   NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE,
-    FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE
+    FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE,
+    FOREIGN KEY (labeled_data_id) REFERENCES labeled_data (id) ON DELETE CASCADE
 
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4;
@@ -52,55 +67,63 @@ CREATE TABLE audio
     quality         ENUM ('INTEGRATED','DEDICATED')                 DEFAULT NULL,
     noise_level     ENUM ('NO_NOISE','MODERATE_NOISE','VERY_NOISY') DEFAULT NULL,
     browser_version TEXT                                            DEFAULT NULL,
+    labeled_data_id BIGINT   NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE,
-    FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE
+    FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE,
+    FOREIGN KEY (labeled_data_id) REFERENCES labeled_data (id) ON DELETE CASCADE
 
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4;
 
 CREATE TABLE image
 (
-    id           BIGINT   NOT NULL AUTO_INCREMENT,
-    path         TEXT     NOT NULL,
-    created_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    source_id    BIGINT   NOT NULL,
+    id              BIGINT   NOT NULL AUTO_INCREMENT,
+    path            TEXT     NOT NULL,
+    created_time    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    source_id       BIGINT   NOT NULL,
+    labeled_data_id BIGINT   NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE
+    FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE,
+    FOREIGN KEY (labeled_data_id) REFERENCES labeled_data (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4;
-CREATE TABLE skipped_recording
+
+CREATE TABLE checked_data
 (
-    id           BIGINT   NOT NULL AUTO_INCREMENT,
-    created_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    text_1       BIGINT            DEFAULT NULL,
-    user_id      BIGINT   NOT NULL,
+    id           BIGINT                                      NOT NULL AUTO_INCREMENT,
+    created_time DATETIME                                    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    type         enum ('SKIPPED','PRIVATE','SENTENCE_ERROR') NOT NULL,
+    user_id      BIGINT                                      NOT NULL,
+    text_1       BIGINT                                               DEFAULT NULL,
+    image_1      BIGINT                                               DEFAULT NULL,
+    audio_1      BIGINT                                               DEFAULT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
+    FOREIGN KEY (text_1) REFERENCES text (id) ON DELETE CASCADE,
+    FOREIGN KEY (audio_1) REFERENCES audio (id) ON DELETE CASCADE,
+    FOREIGN KEY (image_1) REFERENCES image (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4
-    COMMENT ='this table is used to save the audio,text or images a user has skipped while recording';
+    COMMENT ='this table is used to save the label of a text,audio,image by a user so we can revert it in case a user produces nonsense while recording/transcribing';
 
 CREATE TABLE occurrence
 (
-    id                BIGINT                                                                   NOT NULL AUTO_INCREMENT,
-    type              ENUM ('TEXT_TEXT','AUDIO_AUDIO','TEXT_AUDIO','AUDIO_TEXT','IMAGE_AUDIO') NOT NULL,
-    text_1            BIGINT  DEFAULT NULL,
-    text_2            BIGINT  DEFAULT NULL,
-    audio_1           BIGINT  DEFAULT NULL,
-    audio_2           BIGINT  DEFAULT NULL,
-    image_1           BIGINT  DEFAULT NULL,
-    is_sentence_error BOOLEAN DEFAULT FALSE,
-    is_private        BOOLEAN DEFAULT FALSE,
-    correct           BIGINT  DEFAULT 0,
-    wrong             BIGINT  DEFAULT 0,
-    skipped           BIGINT  DEFAULT 0,
+    id              BIGINT                                                                   NOT NULL AUTO_INCREMENT,
+    type            ENUM ('TEXT_TEXT','AUDIO_AUDIO','TEXT_AUDIO','AUDIO_TEXT','IMAGE_AUDIO') NOT NULL,
+    text_1          BIGINT DEFAULT NULL,
+    text_2          BIGINT DEFAULT NULL,
+    audio_1         BIGINT DEFAULT NULL,
+    audio_2         BIGINT DEFAULT NULL,
+    image_1         BIGINT DEFAULT NULL,
+    labeled_data_id BIGINT                                                                   NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (text_1) REFERENCES text (id) ON DELETE CASCADE,
     FOREIGN KEY (text_2) REFERENCES text (id) ON DELETE CASCADE,
     FOREIGN KEY (audio_1) REFERENCES audio (id) ON DELETE CASCADE,
     FOREIGN KEY (audio_2) REFERENCES audio (id) ON DELETE CASCADE,
-    FOREIGN KEY (image_1) REFERENCES image (id) ON DELETE CASCADE
+    FOREIGN KEY (image_1) REFERENCES image (id) ON DELETE CASCADE,
+    FOREIGN KEY (labeled_data_id) REFERENCES labeled_data (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4
     COMMENT 'this table is used to save the audio,text, images occurrences and the aggregated checked_states.
