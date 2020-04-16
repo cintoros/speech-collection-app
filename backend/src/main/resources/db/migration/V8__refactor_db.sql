0a552848a8a630fb24cf5638d889e956d797bb93
Elias Schorr <elias.schorr@fhnw.ch>
2020-04-16 13:18:55 +0200

    simplify database by using an inheritence approach

--- backend/src/main/resources/db/migration/V8__refactor_db.sql
+++ backend/src/main/resources/db/migration/V8__refactor_db.sql
@@ -1,6 +1,10 @@
 -- refactor database for audio,image,text matching between each other.
 
 -- TODO not sure if it makes sense to migrate or if we want to re-create the database?
+DROP TABLE checked_recording,checked_text_audio;
+DROP TABLE text_audio,recording;
+DROP TABLE source,excerpt;
+DROP TABLE speaker,original_text;
 
 -- TODO probably add triggers to update occurrence counts on insert and delete (or join each time for overviews) once the base data structure is clear.
 create table checked_data
@@ -14,9 +18,7 @@ create table checked_data
     PRIMARY KEY (id)
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4
-    COMMENT 'this table is used to save aggregated checked_states of the audio,text, images and occurrences.';
-
-RENAME TABLE source TO transcript_source;
+    COMMENT 'this table is used to save aggregated checked_states of an element (audio,text,images) and occurrences.';
 
 CREATE TABLE source
 (
@@ -24,7 +26,6 @@ CREATE TABLE source
     user_id          BIGINT   NOT NULL,
     user_group_id    BIGINT   NOT NULL,
     created_time     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    uploaded         BOOLEAN           DEFAULT FALSE,
     path_to_raw_file TEXT              DEFAULT NULL,
     name             TEXT              DEFAULT NULL,
     licence          TEXT              DEFAULT NULL,
@@ -38,77 +39,79 @@ CREATE TABLE source
     FOREIGN KEY (domain_id) REFERENCES domain (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4
-    COMMENT 'the source mappings can be uploaded by user and/or auto generated based on something uploaded by the user.
-or it can be imported/upload by a script etc this is why the more "advanced" fields are only filled in case it was uploaded';
+    COMMENT 'the source mappings can be generated based on something uploaded by the user.
+Or it can be imported/upload by a script etc this is why the more "advanced" fields are only filled in case it was uploaded';
 
-CREATE TABLE text
+CREATE TABLE excerpt
 (
-    id              BIGINT   NOT NULL AUTO_INCREMENT,
-    text            TEXT     NOT NULL,
-    source_id       BIGINT   NOT NULL,
-    language_id     BIGINT   NOT NULL,
-    created_time    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    checked_data_id BIGINT   NOT NULL,
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    text       TEXT   NOT NULL,
+    dialect_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE,
-    FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE,
-    FOREIGN KEY (checked_data_id) REFERENCES checked_data (id) ON DELETE CASCADE
-
+    FOREIGN KEY (dialect_id) REFERENCES dialect (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4;
 
 CREATE TABLE audio
 (
-    id              BIGINT   NOT NULL AUTO_INCREMENT,
-    path            TEXT     NOT NULL,
-    source_id       BIGINT   NOT NULL,
-    language_id     BIGINT   NOT NULL,
-    created_time    DATETIME NOT NULL                               DEFAULT CURRENT_TIMESTAMP,
+    id              BIGINT NOT NULL AUTO_INCREMENT,
+    path            TEXT   NOT NULL,
+    dialect_id      BIGINT NOT NULL,
     quality         ENUM ('INTEGRATED','DEDICATED')                 DEFAULT NULL,
     noise_level     ENUM ('NO_NOISE','MODERATE_NOISE','VERY_NOISY') DEFAULT NULL,
     browser_version TEXT                                            DEFAULT NULL,
-    checked_data_id BIGINT   NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE,
-    FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE,
-    FOREIGN KEY (checked_data_id) REFERENCES checked_data (id) ON DELETE CASCADE
-
+    FOREIGN KEY (dialect_id) REFERENCES dialect (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4;
 
 CREATE TABLE image
 (
-    id              BIGINT   NOT NULL AUTO_INCREMENT,
-    path            TEXT     NOT NULL,
-    created_time    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    source_id       BIGINT   NOT NULL,
-    checked_data_id BIGINT   NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE,
-    FOREIGN KEY (checked_data_id) REFERENCES checked_data (id) ON DELETE CASCADE
+    id      BIGINT NOT NULL AUTO_INCREMENT,
+    path    TEXT   NOT NULL,
+    licence TEXT DEFAULT NULL,
+    PRIMARY KEY (id)
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4;
+
+CREATE TABLE element
+(
+    id              BIGINT   NOT NULL AUTO_INCREMENT,
+    excerpt_id      BIGINT            DEFAULT NULL,
+    audio_id        BIGINT            DEFAULT NULL,
+    image_id        BIGINT            DEFAULT NULL,
+    created_time    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    source_id       BIGINT            DEFAULT NULL,
+    user_id         BIGINT            DEFAULT NULL,
+    checked_data_id BIGINT   NOT NULL,
+    finished        BOOLEAN           DEFAULT FALSE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE,
+    FOREIGN KEY (excerpt_id) REFERENCES excerpt (id) ON DELETE CASCADE,
+    FOREIGN KEY (audio_id) REFERENCES audio (id) ON DELETE CASCADE,
+    FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
+    FOREIGN KEY (checked_data_id) REFERENCES checked_data (id) ON DELETE CASCADE
+) ENGINE = INNODB
+  DEFAULT CHARSET = UTF8MB4 COMMENT 'the element can be an audio,text or image.
+In case of text & audio it can be uploaded normally or generated using a source i.e. transcript, text-document
+The finished flag is used to prevent cycles like excerpt=>recording=>transcript=>recording=>transcript...
+It can also be used to flag an element that already has enough transcripts/recordings';
 
 CREATE TABLE occurrence
 (
     id              BIGINT                                                                   NOT NULL AUTO_INCREMENT,
     type            ENUM ('TEXT_TEXT','AUDIO_AUDIO','TEXT_AUDIO','AUDIO_TEXT','IMAGE_AUDIO') NOT NULL,
-    text_1          BIGINT DEFAULT NULL,
-    text_2          BIGINT DEFAULT NULL,
-    audio_1         BIGINT DEFAULT NULL,
-    audio_2         BIGINT DEFAULT NULL,
-    image_1         BIGINT DEFAULT NULL,
+    element_id_1    BIGINT                                                                   NOT NULL,
+    element_id_2    BIGINT                                                                   NOT NULL,
     checked_data_id BIGINT                                                                   NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (text_1) REFERENCES text (id) ON DELETE CASCADE,
-    FOREIGN KEY (text_2) REFERENCES text (id) ON DELETE CASCADE,
-    FOREIGN KEY (audio_1) REFERENCES audio (id) ON DELETE CASCADE,
-    FOREIGN KEY (audio_2) REFERENCES audio (id) ON DELETE CASCADE,
-    FOREIGN KEY (image_1) REFERENCES image (id) ON DELETE CASCADE,
+    FOREIGN KEY (element_id_1) REFERENCES element (id) ON DELETE CASCADE,
+    FOREIGN KEY (element_id_2) REFERENCES element (id) ON DELETE CASCADE,
     FOREIGN KEY (checked_data_id) REFERENCES checked_data (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4
-    COMMENT 'this table is used to save the audio,text, images occurrences and the aggregated checked_states.
+    COMMENT 'this table is used to save the audio,text, images occurrences.
 the type describes which foreign keys are set i.e TEXT_AUDIO(text_1,audio_2),IMAGE_AUDIO(image_1,audio_2)';
 
 CREATE TABLE checked_data_user
@@ -127,10 +130,3 @@ CREATE TABLE checked_data_user
 
 ALTER TABLE user_group
     ADD COLUMN meta_information JSON DEFAULT NULL COMMENT 'contains meta_information. for example which features are activated per user_group';
-
-# TODO maybe implement data migration here before dropping the old tables
-
-DROP TABLE checked_recording,checked_text_audio;
-DROP TABLE text_audio,recording;
-DROP TABLE transcript_source,excerpt;
-DROP TABLE speaker,original_text;
