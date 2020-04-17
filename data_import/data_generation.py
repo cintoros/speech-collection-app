76cac7056fac6c6829bba46c829487f1a506ce7c
Elias Schorr <elias.schorr@fhnw.ch>
2020-04-17 14:56:59 +0200

    add acapela links and change pyttsx3

--- data_import/data_generation.py
+++ data_import/data_generation.py
@@ -11,18 +11,15 @@ def ttsGTTS(text: str):
     tts.save(os.path.join(base_dir, 'data_generation', "gTTS.mp3"))
 
 
-# requires espeak , nsss or sapi5 to be installed locally
+# requires espeak(all) , nsss(mac) or sapi5(windows) to be installed locally
+# TODO not sure about the quality
 def ttsPyttsx3(text: str):
     engine = pyttsx3.init()
-    voices: Iterable = engine.getProperty('voices')
-    for voice in voices:
-        print(voice)
-    # TODO speech rate/volume can be changed for additional data
     engine.setProperty('voice', 'german')
-    rate: int = engine.getProperty('rate')
-    engine.setProperty('rate', rate - 0)
-    volume: int = engine.getProperty('volume')
-    engine.setProperty('volume', volume - 0.0)
+    # rate: int = engine.getProperty('rate')
+    # engine.setProperty('rate', rate - 0)
+    # volume: int = engine.getProperty('volume')
+    # engine.setProperty('volume', volume - 0.0)
     print("save_to_file")
     engine.save_to_file(text, os.path.join(base_dir, 'data_generation', "pyttsx3.mp3"))
     print("runAndWait")
@@ -30,10 +27,16 @@ def ttsPyttsx3(text: str):
     print("finished?")
 
 
+# https://acapela-box.com/AcaBox/index.php does not serve an api for a cloud base api see https://www.acapela-group.com/solutions/acapela-vaas/
+def acapela(text: str):
+    print("not implemented")
+
+
 def run(text: str):
     # ttsGTTS(text)
     # ttsPyttsx3(text)
+    acapela(text)
 
 
 if __name__ == '__main__':
-    run("Hallo Welt das hier ist ein deutscher Text gesprochen von einem Text to Speech Programm.")
+    run("Hallo Welt das hier ist ein deutscher Text gesprochen von einem Programm.")
