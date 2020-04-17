d04aabbad0adbbcd933f19543f6a4bf1f9953ebe
Elias Schorr <elias.schorr@fhnw.ch>
2020-04-17 13:10:42 +0200

    split checked_data into checked_data_element & checked_data_tuple

--- backend/src/main/resources/db/migration/V8__refactor_db.sql
+++ backend/src/main/resources/db/migration/V8__refactor_db.sql
@@ -6,79 +6,76 @@ DROP TABLE text_audio,recording;
 DROP TABLE source,excerpt;
 DROP TABLE speaker,original_text;
 
-create table checked_data
-(
-    id                BIGINT NOT NULL AUTO_INCREMENT,
-    is_sentence_error BOOLEAN DEFAULT FALSE,
-    is_private        BOOLEAN DEFAULT FALSE,
-    correct           BIGINT  DEFAULT 0,
-    wrong             BIGINT  DEFAULT 0,
-    skipped           BIGINT  DEFAULT 0,
-    PRIMARY KEY (id)
-) ENGINE = INNODB
-  DEFAULT CHARSET = UTF8MB4
-    COMMENT 'this table is used to save aggregated checked_states of an data_element and data_tuples.';
+ALTER TABLE user_group
+    ADD COLUMN meta_information JSON DEFAULT NULL COMMENT 'contains meta_information. for example which features are activated per user_group';
+
+INSERT INTO dialect(county_id, county_name, language_id) VALUE ('DE-de', 'Standard German', 2);
 
 CREATE TABLE source
 (
     id               BIGINT   NOT NULL AUTO_INCREMENT,
     user_id          BIGINT   NOT NULL,
-    user_group_id    BIGINT   NOT NULL,
+    dialect_id       BIGINT            DEFAULT NULL,
+    domain_id        BIGINT            DEFAULT NULL,
     created_time     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     path_to_raw_file TEXT              DEFAULT NULL,
     name             TEXT              DEFAULT NULL,
     licence          TEXT              DEFAULT NULL,
-    dialect_id       BIGINT            DEFAULT NULL,
-    domain_id        BIGINT            DEFAULT NULL,
     meta_information JSON              DEFAULT NULL COMMENT 'contains meta_information that are different depending on the source etc.',
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
-    FOREIGN KEY (user_group_id) REFERENCES user_group (id) ON DELETE CASCADE,
     FOREIGN KEY (dialect_id) REFERENCES dialect (id) ON DELETE CASCADE,
     FOREIGN KEY (domain_id) REFERENCES domain (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4
-    COMMENT 'the source mappings can be generated based on something uploaded by the user.
-Or it can be imported/upload by a script etc this is why the more "advanced" fields are only filled in case it was uploaded';
+    COMMENT 'a source can be generated based on something uploaded by the user i.e a text document.
+Or it can be imported by a script this is why most fields are optional.';
 
 CREATE TABLE data_element
 (
-    id              BIGINT   NOT NULL AUTO_INCREMENT,
-    created_time    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    source_id       BIGINT            DEFAULT NULL,
-    user_id         BIGINT            DEFAULT NULL,
-    checked_data_id BIGINT   NOT NULL,
-    finished        BOOLEAN           DEFAULT FALSE,
+    id                BIGINT   NOT NULL AUTO_INCREMENT,
+    source_id         BIGINT            DEFAULT NULL,
+    user_id           BIGINT            DEFAULT NULL,
+    user_group_id     BIGINT   NOT NULL,
+    created_time      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    finished          BOOLEAN           DEFAULT FALSE,
+    is_sentence_error BOOLEAN           DEFAULT FALSE,
+    is_private        BOOLEAN           DEFAULT FALSE,
+    skipped           BIGINT            DEFAULT 0,
     PRIMARY KEY (id),
     FOREIGN KEY (source_id) REFERENCES source (id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
-    FOREIGN KEY (checked_data_id) REFERENCES checked_data (id) ON DELETE CASCADE
+    FOREIGN KEY (user_group_id) REFERENCES user_group (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4 COMMENT 'the data_element can be an audio,text or image.
 In case of text & audio it can be uploaded normally or generated using a source i.e. transcript, text-document
-The finished flag is used to prevent cycles like text=>recording=>transcript=>recording=>transcript...
-It can also be used to flag an data_element that already has enough transcripts/recordings';
+The finished flag is used to prevent cycles like text=>recording=>text=>recording=>...
+It can also be used to flag an data_element that already has enough recordings etc.';
 
 CREATE TABLE text
 (
-    id         BIGINT NOT NULL AUTO_INCREMENT,
-    text       TEXT   NOT NULL,
-    dialect_id BIGINT NOT NULL,
+    id              BIGINT NOT NULL AUTO_INCREMENT,
+    dialect_id      BIGINT NOT NULL,
+    data_element_id BIGINT NOT NULL,
+    text            TEXT   NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (dialect_id) REFERENCES dialect (id) ON DELETE CASCADE
+    FOREIGN KEY (dialect_id) REFERENCES dialect (id) ON DELETE CASCADE,
+    FOREIGN KEY (data_element_id) REFERENCES data_element (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4;
 
 CREATE TABLE audio
 (
     id              BIGINT NOT NULL AUTO_INCREMENT,
-    path            TEXT   NOT NULL,
     dialect_id      BIGINT NOT NULL,
+    data_element_id BIGINT NOT NULL,
+    path            TEXT   NOT NULL,
     quality         ENUM ('INTEGRATED','DEDICATED')                 DEFAULT NULL,
     noise_level     ENUM ('NO_NOISE','MODERATE_NOISE','VERY_NOISY') DEFAULT NULL,
     browser_version TEXT                                            DEFAULT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (dialect_id) REFERENCES dialect (id) ON DELETE CASCADE
+    FOREIGN KEY (dialect_id) REFERENCES dialect (id) ON DELETE CASCADE,
+    FOREIGN KEY (data_element_id) REFERENCES data_element (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4;
 
@@ -96,98 +93,135 @@ CREATE TABLE image
 CREATE TABLE data_tuple
 (
     id                BIGINT                                                                   NOT NULL AUTO_INCREMENT,
-    type              ENUM ('TEXT_TEXT','AUDIO_AUDIO','TEXT_AUDIO','AUDIO_TEXT','IMAGE_AUDIO') NOT NULL,
     data_element_id_1 BIGINT                                                                   NOT NULL,
     data_element_id_2 BIGINT                                                                   NOT NULL,
-    checked_data_id   BIGINT                                                                   NOT NULL,
+    type              ENUM ('TEXT_TEXT','AUDIO_AUDIO','TEXT_AUDIO','AUDIO_TEXT','IMAGE_AUDIO') NOT NULL,
     finished          BOOLEAN DEFAULT FALSE,
+    correct           BIGINT  DEFAULT 0,
+    wrong             BIGINT  DEFAULT 0,
+    skipped           BIGINT  DEFAULT 0,
     PRIMARY KEY (id),
     FOREIGN KEY (data_element_id_1) REFERENCES data_element (id) ON DELETE CASCADE,
-    FOREIGN KEY (data_element_id_2) REFERENCES data_element (id) ON DELETE CASCADE,
-    FOREIGN KEY (checked_data_id) REFERENCES checked_data (id) ON DELETE CASCADE
+    FOREIGN KEY (data_element_id_2) REFERENCES data_element (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4
     COMMENT 'this table is used to save the audio,text, images data_tuples.
 the type describes which foreign keys are set i.e TEXT_AUDIO(text_1,audio_2),IMAGE_AUDIO(image_1,audio_2).
-The finished can be used to flag an data_element that already has enough checks or should not be checked at all.';
+the finished can be used to flag an data_element that already has enough checks or should not be checked at all.';
 
-CREATE TABLE checked_data_user
+CREATE TABLE checked_data_element
 (
-    id              BIGINT                                                        NOT NULL AUTO_INCREMENT,
-    type            enum ('SKIPPED','CORRECT','WRONG','PRIVATE','SENTENCE_ERROR') NOT NULL,
-    created_time    DATETIME                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    user_id         BIGINT                                                        NOT NULL,
-    checked_data_id BIGINT                                                        NOT NULL,
+    id              BIGINT                                      NOT NULL AUTO_INCREMENT,
+    user_id         BIGINT                                      NOT NULL,
+    data_element_id BIGINT                                      NOT NULL,
+    type            ENUM ('SKIPPED','PRIVATE','SENTENCE_ERROR') NOT NULL,
+    created_time    DATETIME                                    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
-    FOREIGN KEY (checked_data_id) REFERENCES checked_data (id) ON DELETE CASCADE
+    FOREIGN KEY (data_element_id) REFERENCES data_element (id) ON DELETE CASCADE
+) ENGINE = INNODB
+  DEFAULT CHARSET = UTF8MB4
+    COMMENT 'this table is used to save the label of a data_element by a user so we can revert it in case a user produces nonsense';
+
+CREATE TABLE checked_data_tuple
+(
+    id            BIGINT                                                        NOT NULL AUTO_INCREMENT,
+    user_id       BIGINT                                                        NOT NULL,
+    data_tuple_id BIGINT                                                        NOT NULL,
+    type          ENUM ('SKIPPED','CORRECT','WRONG','PRIVATE','SENTENCE_ERROR') NOT NULL,
+    created_time  DATETIME                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
+    FOREIGN KEY (data_tuple_id) REFERENCES data_tuple (id) ON DELETE CASCADE
 ) ENGINE = INNODB
   DEFAULT CHARSET = UTF8MB4
     COMMENT 'this table is used to save the label of a data_tuple by a user so we can revert it in case a user produces nonsense';
 
-ALTER TABLE user_group
-    ADD COLUMN meta_information JSON DEFAULT NULL COMMENT 'contains meta_information. for example which features are activated per user_group';
-
 DELIMITER //
 
-CREATE TRIGGER checked_data_user_insert_trigger
+CREATE TRIGGER checked_data_element_insert_trigger
     AFTER INSERT
-    ON checked_data_user
+    ON checked_data_element
     FOR EACH ROW
 BEGIN
-    IF NEW.type = 'CORRECT' THEN
-        UPDATE checked_data
-        SET checked_data.correct = checked_data.correct + 1
-        WHERE checked_data.id = NEW.checked_data_id;
-    elseif NEW.type = 'WRONG' THEN
-        UPDATE checked_data
-        SET checked_data.wrong = checked_data.wrong + 1
-        WHERE checked_data.id = NEW.checked_data_id;
-    elseif NEW.type = 'SKIPPED' THEN
-        UPDATE checked_data
-        SET checked_data.skipped = checked_data.skipped + 1
-        WHERE checked_data.id = NEW.checked_data_id;
+    IF NEW.type = 'SKIPPED' THEN
+        UPDATE data_element
+        SET data_element.skipped = data_element.skipped + 1
+        WHERE data_element.id = NEW.data_element_id;
     elseif NEW.type = 'SENTENCE_ERROR' THEN
-        UPDATE checked_data
-        SET checked_data.is_sentence_error = true
-        WHERE checked_data.id = NEW.checked_data_id;
+        UPDATE data_element
+        SET data_element.is_sentence_error = true
+        WHERE data_element.id = NEW.data_element_id;
     elseif NEW.type = 'PRIVATE' THEN
-        UPDATE checked_data
-        SET checked_data.is_private = true
-        WHERE checked_data.id = NEW.checked_data_id;
+        UPDATE data_element
+        SET data_element.is_private = true
+        WHERE data_element.id = NEW.data_element_id;
     END IF;
 END;
 //
 
-CREATE TRIGGER checked_data_user_delete_trigger
+CREATE TRIGGER checked_data_element_delete_trigger
     AFTER DELETE
-    ON checked_data_user
+    ON checked_data_element
+    FOR EACH ROW
+BEGIN
+    IF OLD.type = 'SKIPPED' THEN
+        UPDATE data_element
+        SET data_element.skipped = data_element.skipped - 1
+        WHERE data_element.id = OLD.data_element_id;
+    elseif OLD.type = 'SENTENCE_ERROR' THEN
+        UPDATE data_element
+        SET data_element.is_sentence_error = false
+        WHERE data_element.id = OLD.data_element_id;
+    elseif OLD.type = 'PRIVATE' THEN
+        UPDATE data_element
+        SET data_element.is_private = false
+        WHERE data_element.id = OLD.data_element_id;
+    END IF;
+END;
+//
+
+
+CREATE TRIGGER checked_data_tuple_insert_trigger
+    AFTER INSERT
+    ON checked_data_tuple
+    FOR EACH ROW
+BEGIN
+    IF NEW.type = 'CORRECT' THEN
+        UPDATE data_tuple
+        SET data_tuple.correct = data_tuple.correct + 1
+        WHERE data_tuple.id = NEW.data_tuple_id;
+    elseif NEW.type = 'WRONG' THEN
+        UPDATE data_tuple
+        SET data_tuple.wrong = data_tuple.wrong + 1
+        WHERE data_tuple.id = NEW.data_tuple_id;
+    elseif NEW.type = 'SKIPPED' THEN
+        UPDATE data_tuple
+        SET data_tuple.skipped = data_tuple.skipped + 1
+        WHERE data_tuple.id = NEW.data_tuple_id;
+    END IF;
+END;
+//
+
+CREATE TRIGGER checked_data_tuple_delete_trigger
+    AFTER DELETE
+    ON checked_data_tuple
     FOR EACH ROW
 BEGIN
     IF OLD.type = 'CORRECT' THEN
-        UPDATE checked_data
-        SET checked_data.correct = checked_data.correct - 1
-        WHERE checked_data.id = OLD.checked_data_id;
+        UPDATE data_tuple
+        SET data_tuple.correct = data_tuple.correct - 1
+        WHERE data_tuple.id = OLD.data_tuple_id;
     elseif OLD.type = 'WRONG' THEN
-        UPDATE checked_data
-        SET checked_data.wrong = checked_data.wrong - 1
-        WHERE checked_data.id = OLD.checked_data_id;
+        UPDATE data_tuple
+        SET data_tuple.wrong = data_tuple.wrong - 1
+        WHERE data_tuple.id = OLD.data_tuple_id;
     elseif OLD.type = 'SKIPPED' THEN
-        UPDATE checked_data
-        SET checked_data.skipped = checked_data.skipped - 1
-        WHERE checked_data.id = OLD.checked_data_id;
-    elseif OLD.type = 'SENTENCE_ERROR' THEN
-        UPDATE checked_data
-        SET checked_data.is_sentence_error = false
-        WHERE checked_data.id = OLD.checked_data_id;
-    elseif OLD.type = 'PRIVATE' THEN
-        UPDATE checked_data
-        SET checked_data.is_private = false
-        WHERE checked_data.id = OLD.checked_data_id;
+        UPDATE data_tuple
+        SET data_tuple.skipped = data_tuple.skipped - 1
+        WHERE data_tuple.id = OLD.data_tuple_id;
     END IF;
 END;
 //
 
 DELIMITER ;
-
-INSERT INTO dialect(county_id, county_name, language_id) VALUE ('DE-de', 'Standard German', 2)
