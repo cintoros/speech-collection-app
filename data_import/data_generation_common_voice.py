c9da7e081b4cbb27df39bbcf1f6816f0b9c5b6b5
Elias Schorr <elias.schorr@fhnw.ch>
2020-04-23 16:31:25 +0200

    prepare common voice data

---
+++ data_import/data_generation_common_voice.py
@@ -0,0 +1,16 @@
+import os
+
+from config import base_dir
+
+
+def extractdeCH():
+    sourcePath = os.path.join(base_dir, "common_voice")
+    print("implement once download has finished")
+
+
+def importData():
+    print("implement once download has finished")
+
+
+if __name__ == '__main__':
+    extractdeCH()
