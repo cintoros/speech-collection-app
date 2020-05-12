e321e24b2df95b78a06987604aa2e5bf1a6d3f88
Viturin Züst <vzuest@ethz.ch>
2020-05-12 05:31:24 -0700

    prettier

--- frontend/src/app/components/record/excerpt/excerpt.component.ts
+++ frontend/src/app/components/record/excerpt/excerpt.component.ts
@@ -1,12 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-excerpt',
-  templateUrl: './excerpt.component.html',
-  styleUrls: ['./excerpt.component.scss']
+  selector: "app-excerpt",
+  templateUrl: "./excerpt.component.html",
+  styleUrls: ["./excerpt.component.scss"],
 })
-
-
 export class ExcerptComponent implements OnInit {
   constructor() {}
 
--- frontend/src/app/components/record/record.component.ts
+++ frontend/src/app/components/record/record.component.ts
@@ -1,89 +1,136 @@
-import {HttpClient} from '@angular/common/http';
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
-import {Observable, Subscription, timer} from 'rxjs';
-import {ExcerptComponent} from 'src/app/components/record/excerpt/excerpt.component'
+import { HttpClient } from "@angular/common/http";
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import { Observable, Subscription, timer } from "rxjs";
+import { ExcerptComponent } from "src/app/components/record/excerpt/excerpt.component";
 
-    @Component({selector: 'app-record', templateUrl: './record.component.html', styleUrls: ['./record.component.scss']}) export class RecordComponent implements OnInit {textDto: TextDto = null; isRecording = false; blobUrl: SafeUrl; recordingQuality = RecordingQuality; recordingNoiseLevel = RecordingNoiseLevel; selectedQuality = RecordingQuality.INTEGRATED; selectedNoiseLevel = RecordingNoiseLevel.MODERATE_NOISE;
-                                                                                                                                                                         // @ts-ignore
-                                                                                                                                                                         private mediaRecorder: MediaRecorder; private audioChunks = []; private groupId = 1; private elapsedTime = 0; private numberObservable: Observable<number>; private subscription: Subscription;
+@Component({
+  selector: "app-record",
+  templateUrl: "./record.component.html",
+  styleUrls: ["./record.component.scss"],
+})
+export class RecordComponent implements OnInit {
+  textDto: TextDto = null;
+  isRecording = false;
+  blobUrl: SafeUrl;
+  recordingQuality = RecordingQuality;
+  recordingNoiseLevel = RecordingNoiseLevel;
+  selectedQuality = RecordingQuality.INTEGRATED;
+  selectedNoiseLevel = RecordingNoiseLevel.MODERATE_NOISE;
+  // @ts-ignore
+  private mediaRecorder: MediaRecorder;
+  private audioChunks = [];
+  private groupId = 1;
+  private elapsedTime = 0;
+  private numberObservable: Observable<number>;
+  private subscription: Subscription;
 
-                                                                                                                                                                         constructor(private snackBarService: SnackBarService, private detector: ChangeDetectorRef, private httpClient: HttpClient, private domSanitizer: DomSanitizer, private userGroupService: UserGroupService) {
+  constructor(
+    private snackBarService: SnackBarService,
+    private detector: ChangeDetectorRef,
+    private httpClient: HttpClient,
+    private domSanitizer: DomSanitizer,
+    private userGroupService: UserGroupService
+  ) {
     this.groupId = this.userGroupService.userGroupId;
-                                                                                                                                                                         }
+  }
 
-                                                                                                                                                                         ngOnInit() {
+  ngOnInit() {
     this.getNext();
-    navigator.mediaDevices.getUserMedia({audio: true})
-      .then(stream => {
-        // @ts-ignore
-        this.mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'});
-        this.mediaRecorder.ondataavailable = event => this.audioChunks.push(event.data);
-        this.mediaRecorder.onstop = () => {
-          this.blobUrl = this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(new Blob(this.audioChunks)));
-          this.detector.detectChanges();
-        };
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      // @ts-ignore
+      this.mediaRecorder = new MediaRecorder(stream, {
+        mimeType: "audio/webm",
       });
+      this.mediaRecorder.ondataavailable = (event) =>
+        this.audioChunks.push(event.data);
+      this.mediaRecorder.onstop = () => {
+        this.blobUrl = this.domSanitizer.bypassSecurityTrustUrl(
+          URL.createObjectURL(new Blob(this.audioChunks))
+        );
+        this.detector.detectChanges();
+      };
+    });
     this.numberObservable = timer(0, 1000);
-                                                                                                                                                                         }
+  }
 
-                                                                                                                                                                         startRecord(): void {
+  startRecord(): void {
     this.audioChunks = [];
     this.mediaRecorder.start();
     this.elapsedTime = 0;
     this.isRecording = true;
-    this.subscription = this.numberObservable.subscribe(value => this.elapsedTime = value);
-                                                                                                                                                                         }
+    this.subscription = this.numberObservable.subscribe(
+      (value) => (this.elapsedTime = value)
+    );
+  }
 
-                                                                                                                                                                         stopRecord(): void {
+  stopRecord(): void {
     this.subscription.unsubscribe();
     this.mediaRecorder.stop();
     this.isRecording = false;
-                                                                                                                                                                         }
+  }
 
-                                                                                                                                                                         submit(): void {
-    const recording = new RecordingDto(this.textDto.id, this.selectedQuality, this.selectedNoiseLevel);
+  submit(): void {
+    const recording = new RecordingDto(
+      this.textDto.id,
+      this.selectedQuality,
+      this.selectedNoiseLevel
+    );
     const formData = new FormData();
-    formData.append(`file`, new Blob(this.audioChunks), 'audio');
-    formData.append('recording', JSON.stringify(recording));
-    this.httpClient.post(`${environment.url}user_group/${this.groupId}/recording`, formData).subscribe(() => {
-      this.audioChunks = [];
-      this.blobUrl = undefined;
-      this.snackBarService.openMessage('Successfully uploaded recording');
-      this.getNext();
-    });
-                                                                                                                                                                         }
+    formData.append(`file`, new Blob(this.audioChunks), "audio");
+    formData.append("recording", JSON.stringify(recording));
+    this.httpClient
+      .post(`${environment.url}user_group/${this.groupId}/recording`, formData)
+      .subscribe(() => {
+        this.audioChunks = [];
+        this.blobUrl = undefined;
+        this.snackBarService.openMessage("Successfully uploaded recording");
+        this.getNext();
+      });
+  }
 
-                                                                                                                                                                         private() {
+  private() {
     this.check(CheckedDataElementType.PRIVATE).subscribe(() => {
-      this.snackBarService.openMessage('marked as private');
+      this.snackBarService.openMessage("marked as private");
       this.textDto.isPrivate = true;
     });
-                                                                                                                                                                         }
+  }
 
-                                                                                                                                                                         skip() {
+  skip() {
     this.check(CheckedDataElementType.SKIPPED).subscribe(() => {
-      this.snackBarService.openMessage('marked as skipped');
+      this.snackBarService.openMessage("marked as skipped");
       this.getNext();
     });
-                                                                                                                                                                         }
+  }
 
-                                                                                                                                                                         sentenceError() {
+  sentenceError() {
     this.check(CheckedDataElementType.SENTENCE_ERROR).subscribe(() => {
       this.snackBarService.openMessage('marked as "Not a sentence"');
       this.getNext();
     });
-                                                                                                                                                                         }
+  }
 
-                                                                                                                                                                         isReady = () => this.audioChunks.length > 0; private check = (type: CheckedDataElementType) => this.httpClient.post<void>(`${environment.url}user_group/${this.groupId}/element/${this.textDto.id}/checked?type=${type}`, {});
+  isReady = () => this.audioChunks.length > 0;
+  private check = (type: CheckedDataElementType) =>
+    this.httpClient.post<void>(
+      `${environment.url}user_group/${this.groupId}/element/${this.textDto.id}/checked?type=${type}`,
+      {}
+    );
 
-                                                                                                                                                                         private getNext() {
-    this.httpClient.get<TextDto>(`${environment.url}user_group/${this.groupId}/excerpt`).subscribe(value => this.textDto = value);
-                                                                                                                                                                         }}
+  private getNext() {
+    this.httpClient
+      .get<TextDto>(`${environment.url}user_group/${this.groupId}/excerpt`)
+      .subscribe((value) => (this.textDto = value));
+  }
+}
 
-import {environment} from '../../../environments/environment';
-import {CheckedDataElementType} from '../../models/checked-data-element-type';
-import {RecordingDto, RecordingNoiseLevel, RecordingQuality} from '../../models/recording-dto';
-import {TextDto} from '../../models/text-dto';
-import {SnackBarService} from '../../services/snack-bar.service';
-import {UserGroupService} from '../../services/user-group.service';
+import { environment } from "../../../environments/environment";
+import { CheckedDataElementType } from "../../models/checked-data-element-type";
+import {
+  RecordingDto,
+  RecordingNoiseLevel,
+  RecordingQuality,
+} from "../../models/recording-dto";
+import { TextDto } from "../../models/text-dto";
+import { SnackBarService } from "../../services/snack-bar.service";
+import { UserGroupService } from "../../services/user-group.service";
