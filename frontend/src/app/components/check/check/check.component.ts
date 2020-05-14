460abf9cf08c5302afdb6b54512e10bfdd134c24
Viturin Züst <vzuest@ethz.ch>
2020-05-14 08:20:38 -0700

    model for tuples

--- frontend/src/app/components/check/check/check.component.ts
+++ frontend/src/app/components/check/check/check.component.ts
@@ -1,27 +1,32 @@
-import {HttpClient} from '@angular/common/http';
-import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {Router} from '@angular/router';
-import {CarouselComponent} from 'ngx-carousel-lib';
+import { HttpClient } from "@angular/common/http";
+import { Component, HostListener, OnInit, ViewChild } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { CarouselComponent } from "ngx-carousel-lib";
 
-import {environment} from '../../../../environments/environment';
-import {AuthService} from '../../../services/auth.service';
-import {SnackBarService} from '../../../services/snack-bar.service';
-import {UserGroupService} from '../../../services/user-group.service';
-import {CheckMoreComponent} from '../check-more/check-more.component';
-import {ShortcutComponent} from '../shortcut/shortcut.component';
+import { environment } from "../../../../environments/environment";
+import { AuthService } from "../../../services/auth.service";
+import { SnackBarService } from "../../../services/snack-bar.service";
+import { UserGroupService } from "../../../services/user-group.service";
+import { CheckMoreComponent } from "../check-more/check-more.component";
+import { ShortcutComponent } from "../shortcut/shortcut.component";
 
-import {CheckedOccurrence, CheckedOccurrenceLabel, Occurrence} from './checked-occurrence';
+import {
+  CheckedOccurrence,
+  CheckedOccurrenceLabel,
+  Occurrence,
+} from "./checked-occurrence";
+import { TupleDto, TupleType } from "src/app/models/tuple-dto";
 
 export enum OccurrenceMode {
-  RECORDING = 'RECORDING',
-  TEXT_AUDIO = 'TEXT_AUDIO'
+  RECORDING = "RECORDING",
+  TEXT_AUDIO = "TEXT_AUDIO",
 }
 
 @Component({
-  selector: 'app-check',
-  templateUrl: './check.component.html',
-  styleUrls: ['./check.component.scss']
+  selector: "app-check",
+  templateUrl: "./check.component.html",
+  styleUrls: ["./check.component.scss"],
 })
 export class CheckComponent implements OnInit {
   isPlaying = false;
@@ -29,35 +34,40 @@ export class CheckComponent implements OnInit {
   occurrences_idx = 0;
   audioProgress = 0;
   checkedOccurrenceLabel = CheckedOccurrenceLabel;
-  @ViewChild('carousel') private carousel: CarouselComponent;
+  @ViewChild("carousel") private carousel: CarouselComponent;
   private audioPlayer = new Audio();
   private isReady = false;
   private userId: number;
   private groupId = 1;
+  tuple: TupleDto;
 
   constructor(
-      private httpClient: HttpClient, private dialog: MatDialog,
-      private authService: AuthService, private router: Router,
-      private userGroupService: UserGroupService,
-      private snackBarService: SnackBarService) {
+    private httpClient: HttpClient,
+    private dialog: MatDialog,
+    private authService: AuthService,
+    private router: Router,
+    private userGroupService: UserGroupService,
+    private snackBarService: SnackBarService
+  ) {
     this.groupId = this.userGroupService.userGroupId;
   }
 
   ngOnInit() {
-    this.authService.getUser().subscribe(
-        user => this.userId = user.principal.user.id);
+    this.authService
+      .getUser()
+      .subscribe((user) => (this.userId = user.principal.user.id));
     this.getTenNonLabeledTextAudios();
   }
 
-  @HostListener('window:keyup', ['$event'])
+  @HostListener("window:keyup", ["$event"])
   keyEvent(event: KeyboardEvent) {
-    if (event.key === 'p') {
+    if (event.key === "p") {
       this.togglePlay();
-    } else if (event.key === 'c') {
+    } else if (event.key === "c") {
       this.setCheckedType(CheckedOccurrenceLabel.CORRECT);
-    } else if (event.key === 'w') {
+    } else if (event.key === "w") {
       this.setCheckedType(CheckedOccurrenceLabel.WRONG);
-    } else if (event.key === 's') {
+    } else if (event.key === "s") {
       this.setCheckedType(CheckedOccurrenceLabel.SKIPPED);
     }
   }
@@ -72,44 +82,50 @@ export class CheckComponent implements OnInit {
       this.stop();
 
       const occurrence = this.occurrences[this.carousel.carousel.activeIndex];
-      if ((checkType === CheckedOccurrenceLabel.SENTENCE_ERROR ||
-           checkType === CheckedOccurrenceLabel.PRIVATE)) {
+      if (
+        checkType === CheckedOccurrenceLabel.SENTENCE_ERROR ||
+        checkType === CheckedOccurrenceLabel.PRIVATE
+      ) {
         this.httpClient
-            .post(
-                `${environment.url}user_group/${this.groupId}/element/${
-                    occurrence.dataElementId_1}/checked?type=${checkType}`,
-                {})
-            .subscribe(
-                value => this.snackBarService.openMessage(
-                    'successfully updated element.'));
+          .post(
+            `${environment.url}user_group/${this.groupId}/element/${occurrence.dataElementId_1}/checked?type=${checkType}`,
+            {}
+          )
+          .subscribe((value) =>
+            this.snackBarService.openMessage("successfully updated element.")
+          );
       } else {
         const cta = new CheckedOccurrence(occurrence.id, checkType);
         this.httpClient
-            .post(
-                `${environment.url}user_group/${this.groupId}/occurrence/check`,
-                cta)
-            .subscribe();
+          .post(
+            `${environment.url}user_group/${this.groupId}/occurrence/check`,
+            cta
+          )
+          .subscribe();
 
         // checkIfFinishedChunk
-        if (this.carousel.carousel.activeIndex ===
-            this.occurrences.length - 1) {
+        if (
+          this.carousel.carousel.activeIndex ===
+          this.occurrences.length - 1
+        ) {
           this.dialog
-              .open(CheckMoreComponent, {width: '500px', disableClose: true})
-              .afterClosed()
-              .subscribe(result => {
-                if (result) {
-                  // reset carousel and load new data
-                  this.carousel.carousel.activeIndex = 0;
-                  this.occurrences = [];
-                  this.getTenNonLabeledTextAudios();
-                } else {
-                  this.router.navigate(['/home']);
-                }
-              });
+            .open(CheckMoreComponent, { width: "500px", disableClose: true })
+            .afterClosed()
+            .subscribe((result) => {
+              if (result) {
+                // reset carousel and load new data
+                this.carousel.carousel.activeIndex = 0;
+                this.occurrences = [];
+                this.getTenNonLabeledTextAudios();
+              } else {
+                this.router.navigate(["/home"]);
+              }
+            });
         } else {
           this.isReady = false;
           this.loadAudioBlob(
-              this.occurrences[this.carousel.carousel.activeIndex + 1]);
+            this.occurrences[this.carousel.carousel.activeIndex + 1]
+          );
           this.carousel.slideNext();
         }
       }
@@ -125,8 +141,11 @@ export class CheckComponent implements OnInit {
     }
   }
 
-  openShortcutDialog = () => this.dialog.open(
-      ShortcutComponent, {width: '500px', disableClose: false});
+  openShortcutDialog = () =>
+    this.dialog.open(ShortcutComponent, {
+      width: "500px",
+      disableClose: false,
+    });
 
   private play() {
     this.audioPlayer.play();
@@ -142,27 +161,33 @@ export class CheckComponent implements OnInit {
 
   private getTenNonLabeledTextAudios() {
     this.httpClient
-        .get<Array<Occurrence>>(
-            `${environment.url}user_group/${this.groupId}/occurrence/next`)
-        .subscribe(textAudios => {
-          this.occurrences = textAudios;
-          if (textAudios.length > 0) {
-            this.loadAudioBlob(textAudios[0]);
-          }
-        });
+      .get<Array<Occurrence>>(
+        `${environment.url}user_group/${this.groupId}/occurrence/next`
+      )
+      .subscribe((textAudios) => {
+        this.occurrences = textAudios;
+        if (textAudios.length > 0) {
+          this.loadAudioBlob(textAudios[0]);
+        }
+      });
+  }
+
+  private getNonLabeledTuple() {
+    this.tuple = new TupleDto(1, 2, TupleType.TEXT_TEXT, 0, 0, 0, 0);
   }
 
   private loadAudioBlob(occurrence: Occurrence): void {
     this.httpClient
-        .get(
-            `${environment.url}user_group/${this.groupId}/occurrence/audio/${
-                occurrence.dataElementId_2}`,
-            {responseType: 'blob'})
-        .subscribe(resp => {
-          this.audioPlayer = new Audio(URL.createObjectURL(resp));
-          this.audioPlayer.onended = () => this.isPlaying = false;
-          this.audioPlayer.ontimeupdate = () => this.audioProgress =
-              (this.audioPlayer.currentTime / this.audioPlayer.duration) * 100;
-        });
+      .get(
+        `${environment.url}user_group/${this.groupId}/occurrence/audio/${occurrence.dataElementId_2}`,
+        { responseType: "blob" }
+      )
+      .subscribe((resp) => {
+        this.audioPlayer = new Audio(URL.createObjectURL(resp));
+        this.audioPlayer.onended = () => (this.isPlaying = false);
+        this.audioPlayer.ontimeupdate = () =>
+          (this.audioProgress =
+            (this.audioPlayer.currentTime / this.audioPlayer.duration) * 100);
+      });
   }
 }
