05f315e2349245462282a2d74c3a8936a8006046
Viturin Züst <vzuest@ethz.ch>
2020-05-17 18:37:09 -0700

    recording changes correctly

--- frontend/src/app/components/record/recording/recording.component.ts
+++ frontend/src/app/components/record/recording/recording.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit, ChangeDetectorRef, Input, Output, EventEmitter } from "@angular/core";
-import { SafeUrl, DomSanitizer } from "@angular/platform-browser";
-import { Subscription, Observable, timer } from "rxjs";
-import { CheckedDataElementType } from "src/app/models/checked-data-element-type";
-import { HttpClient } from "@angular/common/http";
-import { environment } from "src/environments/environment";
-import { UserGroupService } from "src/app/services/user-group.service";
-import { RecordingDto, RecordingQuality, RecordingNoiseLevel } from "src/app/models/recording-dto";
-import { ElementType } from 'src/app/models/element-type';
-import { DataElementDto } from 'src/app/models/data-element-dto';
+import {HttpClient} from '@angular/common/http';
+import {ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {Observable, Subscription, timer} from 'rxjs';
+import {CheckedDataElementType} from 'src/app/models/checked-data-element-type';
+import {DataElementDto} from 'src/app/models/data-element-dto';
+import {ElementType} from 'src/app/models/element-type';
+import {RecordingDto, RecordingNoiseLevel, RecordingQuality} from 'src/app/models/recording-dto';
+import {UserGroupService} from 'src/app/services/user-group.service';
+import {environment} from 'src/environments/environment';
 
 @Component({
-  selector: "app-recording",
-  templateUrl: "./recording.component.html",
-  styleUrls: ["./recording.component.scss"],
+  selector: 'app-recording',
+  templateUrl: './recording.component.html',
+  styleUrls: ['./recording.component.scss'],
 })
 export class RecordingComponent implements OnInit {
   blobUrl: SafeUrl;
-  isRecording = false; //is true when there is an active recording
+  isRecording = false;  // is true when there is an active recording
+  submitable = false;
+
 
   @Input() otherDataElement: DataElementDto;
   @Input() otherElementType: ElementType;
@@ -31,23 +33,23 @@ export class RecordingComponent implements OnInit {
   private numberObservable: Observable<number>;
 
   constructor(
-    private domSanitizer: DomSanitizer,
-    private userGroupService: UserGroupService,
-    private detector: ChangeDetectorRef,
-    private httpClient: HttpClient
-  ) {
+      private domSanitizer: DomSanitizer,
+      private userGroupService: UserGroupService,
+      private detector: ChangeDetectorRef, private httpClient: HttpClient) {
     this.groupId = this.userGroupService.userGroupId;
   }
 
   ngOnInit(): void {
-    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
       // @ts-ignore
       this.mediaRecorder = new MediaRecorder(stream, {
-        mimeType: "audio/webm",
+        mimeType: 'audio/webm',
       });
-      this.mediaRecorder.ondataavailable = (event) => this.audioChunks.push(event.data);
+      this.mediaRecorder.ondataavailable = (event) =>
+          this.audioChunks.push(event.data);
       this.mediaRecorder.onstop = () => {
-        this.blobUrl = this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(new Blob(this.audioChunks)));
+        this.blobUrl = this.domSanitizer.bypassSecurityTrustUrl(
+            URL.createObjectURL(new Blob(this.audioChunks)));
         this.detector.detectChanges();
       };
     });
@@ -55,11 +57,13 @@ export class RecordingComponent implements OnInit {
   }
 
   startRecord(): void {
+    this.submitable = true;
     this.audioChunks = [];
     this.mediaRecorder.start();
     this.elapsedTime = 0;
     this.isRecording = true;
-    this.subscription = this.numberObservable.subscribe((value) => (this.elapsedTime = value));
+    this.subscription =
+        this.numberObservable.subscribe((value) => (this.elapsedTime = value));
   }
 
   stopRecord(): void {
@@ -68,19 +72,28 @@ export class RecordingComponent implements OnInit {
     this.isRecording = false;
   }
 
-  isReady = () => this.audioChunks.length > 0;
+  isReady(): boolean {
+    return this.audioChunks.length > 0;
+  }
 
   submit_recording(): void {
-    const recording = new RecordingDto(-1, -1, -1, "", RecordingQuality.INTEGRATED, RecordingNoiseLevel.MODERATE_NOISE, "", -1, -1);
+    const recording = new RecordingDto(
+        -1, -1, -1, '', RecordingQuality.INTEGRATED,
+        RecordingNoiseLevel.MODERATE_NOISE, '', -1, -1);
     const formData = new FormData();
-    formData.append(`file`, new Blob(this.audioChunks), "audio");
-    formData.append("recording", JSON.stringify(recording));
-    formData.append("otherDataElement", JSON.stringify(this.otherDataElement));
-    formData.append("otherElementType", JSON.stringify(this.otherElementType));
-    this.httpClient.post(`${environment.url}user_group/${this.groupId}/recording`, formData).subscribe(() => {
-      this.audioChunks = [];
-      this.blobUrl = undefined;
-      this.uploaded.emit("complete");
-    });
+    formData.append(`file`, new Blob(this.audioChunks), 'audio');
+    formData.append('recording', JSON.stringify(recording));
+    formData.append('otherDataElement', JSON.stringify(this.otherDataElement));
+    formData.append('otherElementType', JSON.stringify(this.otherElementType));
+    this.httpClient
+        .post(
+            `${environment.url}user_group/${this.groupId}/recording`, formData)
+        .subscribe(() => {
+          this.submitable = false;
+          this.audioChunks = [];
+
+          this.blobUrl = undefined;
+          this.uploaded.emit('complete');
+        });
   }
 }
