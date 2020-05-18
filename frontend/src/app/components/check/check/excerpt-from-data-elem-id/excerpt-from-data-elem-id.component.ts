97cdbea2ccdf717eb3fa391fde97efc462158cf5
Viturin Züst <vzuest@ethz.ch>
2020-05-18 07:43:25 -0700

    easier excerpt component init

---
+++ frontend/src/app/components/check/check/excerpt-from-data-elem-id/excerpt-from-data-elem-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excerpt-from-data-elem-id',
+  templateUrl: './excerpt-from-data-elem-id.component.html',
+  styleUrls: ['./excerpt-from-data-elem-id.component.scss']
+})
+export class ExcerptFromDataElemIdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
