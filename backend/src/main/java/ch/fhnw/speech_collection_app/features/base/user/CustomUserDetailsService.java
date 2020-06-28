460b38ada3a4427ce7d1e17986b35eaa71372bf2
Viturin Züst <vzuest@ethz.ch>
2020-06-28 05:22:37 -0700

    removed dependency

--- backend/src/main/java/ch/fhnw/speech_collection_app/features/base/user/CustomUserDetailsService.java
+++ backend/src/main/java/ch/fhnw/speech_collection_app/features/base/user/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package ch.fhnw.speech_collection_app.features.base.user;
 
-import ch.fhnw.speech_collection_app.features.base.user_group.AchievementsService;
 import ch.fhnw.speech_collection_app.features.email.EmailSenderService;
 import ch.fhnw.speech_collection_app.jooq.enums.UserGroupRoleRole;
 import ch.fhnw.speech_collection_app.jooq.tables.pojos.*;
