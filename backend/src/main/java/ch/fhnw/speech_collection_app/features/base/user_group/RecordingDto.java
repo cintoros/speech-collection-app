dfa297f3d01a0653f494a1b9039a74657150416a
Elias Schorr <elias.schorr@fhnw.ch>
2020-07-13 16:03:16 +0200

    change flyway versions & fix backend compile

--- backend/src/main/java/ch/fhnw/speech_collection_app/features/base/user_group/RecordingDto.java
+++ backend/src/main/java/ch/fhnw/speech_collection_app/features/base/user_group/RecordingDto.java
@@ -3,33 +3,50 @@ package ch.fhnw.speech_collection_app.features.base.user_group;
 import ch.fhnw.speech_collection_app.jooq.enums.AudioNoiseLevel;
 import ch.fhnw.speech_collection_app.jooq.enums.AudioQuality;
 
-import java.util.*;
-
+//TODO maybe make final
 public class RecordingDto {
-  private Long excerptId;
-  private AudioQuality audioQuality;
-  private AudioNoiseLevel audioNoiseLevel;
+    private Long excerptId;
+    private AudioQuality audioQuality;
+    private AudioNoiseLevel audioNoiseLevel;
+    private String browserVersion;
 
-  public RecordingDto(Long excerptId, AudioQuality audioQuality,
-                      AudioNoiseLevel audioNoiseLevel) {
-    this.excerptId = excerptId;
-    this.audioQuality = audioQuality;
-    this.audioNoiseLevel = audioNoiseLevel;
-  }
+    public RecordingDto(Long excerptId, AudioQuality audioQuality,
+                        AudioNoiseLevel audioNoiseLevel, String browserVersion) {
+        this.excerptId = excerptId;
+        this.audioQuality = audioQuality;
+        this.audioNoiseLevel = audioNoiseLevel;
+        this.browserVersion = browserVersion;
+    }
 
-  public Long getExcerptId() { return excerptId; }
+    public Long getExcerptId() {
+        return excerptId;
+    }
 
-  public void setExcerptId(Long excerptId) { this.excerptId = excerptId; }
+    public void setExcerptId(Long excerptId) {
+        this.excerptId = excerptId;
+    }
 
-  public AudioQuality getAudioQuality() { return audioQuality; }
+    public AudioQuality getAudioQuality() {
+        return audioQuality;
+    }
 
-  public void setAudioQuality(AudioQuality audioQuality) {
-    this.audioQuality = audioQuality;
-  }
+    public void setAudioQuality(AudioQuality audioQuality) {
+        this.audioQuality = audioQuality;
+    }
 
-  public AudioNoiseLevel getAudioNoiseLevel() { return audioNoiseLevel; }
+    public AudioNoiseLevel getAudioNoiseLevel() {
+        return audioNoiseLevel;
+    }
 
-  public void setAudioNoiseLevel(AudioNoiseLevel audioNoiseLevel) {
-    this.audioNoiseLevel = audioNoiseLevel;
-  }
+    public void setAudioNoiseLevel(AudioNoiseLevel audioNoiseLevel) {
+        this.audioNoiseLevel = audioNoiseLevel;
+    }
+
+    public String getBrowserVersion() {
+        return browserVersion;
+    }
+
+    public void setBrowserVersion(String browserVersion) {
+        this.browserVersion = browserVersion;
+    }
 }
