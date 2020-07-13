9423f3a9e48bcb873416127dd133062aa5dc2b17
Elias Schorr <elias.schorr@fhnw.ch>
2020-07-13 17:05:02 +0200

    refactor navigation-menu.component

--- frontend/src/app/components/navigation-menu/navigation-menu.component.ts
+++ frontend/src/app/components/navigation-menu/navigation-menu.component.ts
@@ -1,8 +1,7 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSidenav} from '@angular/material/sidenav';
 import {Router} from '@angular/router';
 import {NumAchievementsService} from 'src/app/services/num-achievements.service';
-
 import {CustomUserDetails, UserGroupRoleRole} from '../../models/spring-principal';
 import {AuthService} from '../../services/auth.service';
 import {UserGroupService} from '../../services/user-group.service';
@@ -12,15 +11,15 @@ import {UserGroupService} from '../../services/user-group.service';
   templateUrl: './navigation-menu.component.html',
   styleUrls: ['./navigation-menu.component.scss']
 })
-export class NavigationMenuComponent {
+export class NavigationMenuComponent implements OnInit {
   @ViewChild('sidenav', {static: true}) sidenav: MatSidenav;
   user: CustomUserDetails;
   message: string;
 
   constructor(
-      public authService: AuthService, public router: Router,
-      private userGroupService: UserGroupService,
-      private numAchievementsService: NumAchievementsService) {
+    public authService: AuthService, public router: Router,
+    private userGroupService: UserGroupService,
+    private numAchievementsService: NumAchievementsService) {
     authService.getUser().subscribe((user) => {
       this.user = user.principal;
       this.user.gamificationOn = user.principal.user.gamificationOn;
@@ -29,7 +28,7 @@ export class NavigationMenuComponent {
 
   ngOnInit(): void {
     this.numAchievementsService.currentMessage.subscribe(
-        message => this.message = message);
+      message => this.message = message);
     this.numAchievementsService.getNumber();
   }
 
@@ -40,13 +39,13 @@ export class NavigationMenuComponent {
 
   isGroupAdmin() {
     return this.user &&
-        (this.isAdmin() ||
-         this.user.userGroupRoles.find(
-             a => a.userGroupId === this.userGroupService.userGroupId &&
-                 a.role === UserGroupRoleRole.GROUP_ADMIN));
+      (this.isAdmin() ||
+        this.user.userGroupRoles.find(
+          a => a.userGroupId === this.userGroupService.userGroupId &&
+            a.role === UserGroupRoleRole.GROUP_ADMIN));
   }
 
   toggleSidenav = () => this.sidenav.toggle();
   isAdmin = () => this.user &&
-      this.user.userGroupRoles.find(a => a.role === UserGroupRoleRole.ADMIN);
+    this.user.userGroupRoles.find(a => a.role === UserGroupRoleRole.ADMIN);
 }
